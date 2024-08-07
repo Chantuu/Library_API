337602e39eb13eb0ba2af8f4d26ef2154bfb8283
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-07 23:42:04 +0400

    Document customValidators.js

--- utilities/customValidators.js
+++ utilities/customValidators.js
@@ -1,6 +1,12 @@
 const bookRepository = require("../repositories/bookRepository");
 
-
+/**
+ * This validator checks for document existence by supplied bookId parameter.
+ * If corresponding document exists, true is returned, else false.
+ *
+ * @param {ObjectId} bookId Book document id
+ * @returns {Promise<boolean>} Boolean
+ */
 async function validateBookId(bookId) {
     const result = await bookRepository.getBookById(bookId);
     await bookRepository.disconnectFromDb();
