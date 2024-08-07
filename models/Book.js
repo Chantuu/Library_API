5e6b6c1f0356d877b8102df9480b9df2ef8d15ca
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-07 23:01:32 +0400

    Document book.js

--- models/Book.js
+++ models/Book.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+/**
+ * This schema represents a book document in the database.
+ *
+ * @type {module:mongoose.Schema<any, Model<any, any, any, any>, {}, {}, {}, {}, DefaultSchemaOptions, {publishYear: {type: NumberConstructor, required: boolean}, author: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, name: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, genre: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, description: StringConstructor}, HydratedDocument<FlatRecord<{publishYear: {type: NumberConstructor, required: boolean}, author: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, name: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, genre: {maxlength: (number|string)[], minLength: (number|string)[], type: StringConstructor, required: boolean}, description: StringConstructor}>, {}>>}
+ */
 const bookSchema = new mongoose.Schema({
     name: {
         type: String,
