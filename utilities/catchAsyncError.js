85a342b2a695773f4cd49c2aef320c14bfc23d35
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-07 23:40:07 +0400

    Document catchAsyncError.js

--- utilities/catchAsyncError.js
+++ utilities/catchAsyncError.js
@@ -1,3 +1,14 @@
+/**
+ * This method is used for handling errors in async functions.
+ * It takes desired function as callback and immediately returns
+ * new function, where req, res and next parameters get arguments
+ * from app/route methods. As returned function returns, it automatically
+ * triggers callback function. .catch is used to catch any errors and send
+ * them to error handlers with next.
+ *
+ * @param {Function} callback Desired function
+ * @returns {(function(import('express').Request, import('express').Response, callback): void)|*}
+ */
 function catchAsyncError(callback) {
     return function (req, res, next) {
         callback(req, res, next).catch(err => next(err));
