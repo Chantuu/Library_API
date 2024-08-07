824d325e9a4273a720c814467c2c1890984cffe2
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-07 20:39:05 +0400

    Implement error JSON response creator function

--- utilities/jsonResponeCreator.js
+++ utilities/jsonResponeCreator.js
@@ -9,5 +9,19 @@ function createBookJsonResponse(book) {
     return createBaseJsonResponse('success', book);
 }
 
+function createErrorResponse(errorMessage, type, statusCode, innerErrors=[]) {
+    const resultObj = {
+        errorMessage: errorMessage,
+        type: type,
+        statusCode: statusCode
+    };
+
+    if (innerErrors.length) {
+        resultObj.innerErrors = innerErrors;
+    }
+
+    return createBaseJsonResponse('error', resultObj);
+}
 
 module.exports.createBookJsonResponse = createBookJsonResponse;
+module.exports.createErrorResponse = createErrorResponse;
