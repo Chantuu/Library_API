299e3f2ba4b22cbd70a297a067eb666619fed507
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-08 00:02:17 +0400

    Document ValidationError.js

--- utilities/ValidationError.js
+++ utilities/ValidationError.js
@@ -1,4 +1,16 @@
+/**
+ * This error class extends base Error class
+ * and is used for representing any validation errors
+ * coming from express-validator.
+ */
 class ValidationError extends Error {
+    /**
+     * Class constructor
+     *
+     * @param {String} message Error message
+     * @param {Number} statusCode HTTP status code
+     * @param {Array} innerValidationErrors Array of any additional errors (optional)
+     */
     constructor(message, statusCode, innerValidationErrors) {
         super(message);
         this.statusCode = statusCode;
