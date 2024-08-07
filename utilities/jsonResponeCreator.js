2054f00642e3f7328f299befb83d2e6d60bfd3d9
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-08 00:01:37 +0400

    Document jsonResponeCreator.js

--- utilities/jsonResponeCreator.js
+++ utilities/jsonResponeCreator.js
@@ -1,3 +1,10 @@
+/**
+ * This method is used for creating JSON response template.
+ *
+ * @param {String} state
+ * @param {Object} resultObj
+ * @returns {{result: string, state: Object}}
+ */
 function createBaseJsonResponse(state, resultObj) {
     return {
         state: state,
@@ -5,10 +12,28 @@ function createBaseJsonResponse(state, resultObj) {
     }
 }
 
+/**
+ * This method creates new successful JSON representative JavaScript object
+ * containing a book document or an array of book documents in result field.
+ *
+ * @param {import('../models/Book') || [import('../models/Book')]} book Book document or array of book documents
+ * @returns {{result: string, state: import('../models/Book') || [import('../models/Book')]}}
+ */
 function createBookJsonResponse(book) {
     return createBaseJsonResponse('success', book);
 }
 
+/**
+ *
+ * This method creates new error JSON representative Javascript
+ * object containing an error in result field.
+ *
+ * @param {String} errorMessage Error message
+ * @param {String} type Type of error
+ * @param {Number} statusCode HTTP status code
+ * @param {Array} innerErrors Any additional errors (optional)
+ * @returns {{result: string, state: Object}}
+ */
 function createErrorResponse(errorMessage, type, statusCode, innerErrors=[]) {
     const resultObj = {
         errorMessage: errorMessage,
