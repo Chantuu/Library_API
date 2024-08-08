3a001f3265b12c160bdea1ede377d30f58bee50e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-08 19:16:30 +0400

    Add validation for requests content-type

--- utilities/customValidators.js
+++ utilities/customValidators.js
@@ -1,4 +1,6 @@
 const bookRepository = require("../repositories/bookRepository");
+const ValidationError = require("./ValidationError");
+const {validationContentTypeErrorMessage} = require("./errorMessages")
 
 /**
  * This validator checks for document existence by supplied bookId parameter.
@@ -19,5 +21,33 @@ async function validateBookId(bookId) {
     }
 }
 
+/**
+ * This route validates incoming requests content type. It returns function
+ * for express middleware consumption and checks if allowed content type is same as
+ * incoming requests content type. If false, throws ValidationError.
+ *
+ * @param {string} allowedContentType Allowed content type for the route
+ * @returns {(function((import('express').request), (import('express').response), function): void)|*}
+ * @throws {ValidationError}
+ */
+function validateContentType(allowedContentType) {
+    return function (req, res, next) {
+        if (allowedContentType === 'none') {
+            if (!req.get('content-type')) {
+                next();
+            }
+            else {
+                throw new ValidationError(validationContentTypeErrorMessage, 400);
+            }
+        }
+        else if (allowedContentType === req.get('content-type')) {
+            next();
+        }
+        else {
+            throw new ValidationError(validationContentTypeErrorMessage, 400);
+        }
+    }
+}
 
 module.exports.validateBookExists = validateBookId;
+module.exports.validateContentType = validateContentType;
