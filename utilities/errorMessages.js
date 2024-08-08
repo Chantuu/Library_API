ca6b53a533b3a23ea09e76776ac61863d2f6c00e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-08 19:05:49 +0400

    Add new error message in errorMessages.js

--- utilities/errorMessages.js
+++ utilities/errorMessages.js
@@ -1,2 +1,4 @@
 module.exports.validationJsonErrorMessage = 'Given JSON is incorrectly formatted or missing some information.';
 module.exports.validationIdErrorMessage = "The book by that ID doesn't exist. Please type correct ID.";
+module.exports.validationContentTypeErrorMessage
+    = 'The specified content type is not supported by that route. Please read docs to choose correct content type.';
