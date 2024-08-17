7d7f4c2f08349d4cb2f0d99240ea3c30a6da50ad
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-18 03:07:59 +0400

    Add validateUserNotExists function in customValidators.js

--- utilities/customValidators.js
+++ utilities/customValidators.js
@@ -1,7 +1,11 @@
 const bookRepository = require("../repositories/bookRepository");
-const {ValidationError, NotFoundError} = require("./errors");
-const {validationContentTypeErrorMessage, validationIdErrorMessage, validationIdFormatErrorMessage} = require("./errorMessages")
+const {ValidationError, NotFoundError, AlreadyExistsError} = require("./errors");
+const {validationContentTypeErrorMessage, validationIdErrorMessage, validationIdFormatErrorMessage,
+    validationJsonErrorMessage, userAlreadyExistsErrorMessage
+} = require("./errorMessages")
 const mongoose = require("mongoose");
+const UserRepository = require("../repositories/userRepository");
+const {validationResult} = require("express-validator");
 
 
 /**
@@ -62,5 +66,41 @@ function validateContentType(allowedContentType) {
     }
 }
 
+/**
+ * This validator is used to validate, that a User
+ * does not exist in the database. It also checks for
+ * express-validator validations. If express-validator validations
+ * are passed and user does not exist, this validator is validated.
+ * If express-validator validations fail, it throws ValidationError.
+ * If user exists, it throws AlreadyExistsError.
+ *
+ * @param {import('express').request} req Express Request Object
+ * @param {import('express').response} res Express Response Object
+ * @param {function} next Next middleware function
+ * @returns {Promise<void>}
+ * @throws {ValidationError, AlreadyExistsError}
+ */
+async function validateUserNotExists(req, res, next) {
+    const user = await UserRepository.getUserByUsername(req.body.username);
+    await UserRepository.disconnectFromDb();
+
+    const validationRes = validationResult(req);
+
+    // If validations are passed and user by specified username is not found
+    if (!user && validationRes.isEmpty()) {
+        next();
+    }
+    // If validations failed
+    else if (!validationRes.isEmpty()) {
+        throw new ValidationError(validationJsonErrorMessage, validationRes.array({onlyFirstError: true}));
+    }
+    //If user already exists
+    else {
+        throw new AlreadyExistsError(userAlreadyExistsErrorMessage);
+    }
+}
+
+
 module.exports.validateBookExists = validateBookId;
 module.exports.validateContentType = validateContentType;
+module.exports.validateUserNotExists = validateUserNotExists;
