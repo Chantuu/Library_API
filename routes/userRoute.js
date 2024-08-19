cf73a770f4bcb1c409b41515029fca964072023b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2024-08-19 13:55:41 +0400

    Add whitespace validation for user routes in userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,15 +2,16 @@ const express = require('express');
 const {checkExact, body} = require("express-validator");
 const router = express.Router();
 const catchAsyncError = require("../utilities/catchAsyncError");
-const {validateContentType, validateUserNotExists, authenticateUser} = require("../utilities/customValidators");
+const {validateContentType, validateUserNotExists, authenticateUser, noWhitespacesBetween} = require("../utilities/customValidators");
 const {registerNewUser, returnUserData} = require("../controllers/userRouteController");
+const {noSpaceBetweenErrorMessage} = require("../utilities/errorMessages");
 
 
 router.get("/",
     validateContentType('application/json'),
     checkExact([
-        body("username").notEmpty().isString(),
-        body("password").notEmpty().isString(),
+        body("username").notEmpty().isString().custom(noWhitespacesBetween).withMessage(noSpaceBetweenErrorMessage),
+        body("password").notEmpty().isString().custom(noWhitespacesBetween).withMessage(noSpaceBetweenErrorMessage)
     ]),
     catchAsyncError(authenticateUser),
     catchAsyncError(returnUserData));
@@ -18,10 +19,10 @@ router.get("/",
 router.post('/',
     validateContentType('application/json'),
     checkExact([
-        body('username').notEmpty().isString(),
-        body('password').notEmpty().isString(),
-        body('firstName').notEmpty().isString(),
-        body('lastName').notEmpty().isString(),
+        body('username').notEmpty().isString().custom(noWhitespacesBetween).withMessage(noSpaceBetweenErrorMessage),
+        body('password').notEmpty().isString().custom(noWhitespacesBetween).withMessage(noSpaceBetweenErrorMessage),
+        body('firstName').notEmpty().isString().trim(),
+        body('lastName').notEmpty().isString().trim(),
     ]),
     catchAsyncError(validateUserNotExists),
     catchAsyncError(registerNewUser));
