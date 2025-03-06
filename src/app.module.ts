8ee19e4d4248fbebc7768e3773efaff5079e11d6
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-06 14:41:54 +0400

    Import TypeORM Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DATABASE_NAME_PATH,
+      entities: [],
+      synchronize: true,
     }),
   ],
 })
