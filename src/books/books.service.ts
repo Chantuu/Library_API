daa96836a7889c74a30aed0411c6fb93a9c19b0e
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-18 14:30:21 +0400

    Implement Repository fo User Entity
     Implemented Repository for User Entity, which automatically creates corresponding table in the database.

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Book } from './book.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book) private booksRepository: Repository<Book>,
+  ) {}
+}
