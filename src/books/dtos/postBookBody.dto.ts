ee40470cfe71a227b6da4fcbc645c45ee78091a7
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-18 17:13:20 +0400

    Create postBookBodyDTO Validation class
     Created this DTO validation class for validating POST /books endpoint request bodies.

---
+++ src/books/dtos/postBookBody.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class postBookBodyDTO {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
+  title: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  genre: string;
+
+  @IsNumber()
+  publishedYear: number;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
+  author: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(6)
+  description?: string;
+}
