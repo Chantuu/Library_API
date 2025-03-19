5d8a8255b50e39a451181b0300da70e962034a61
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-19 17:54:43 +0400

    Import AuthorsModule in BooksModule

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -4,9 +4,10 @@ import { BooksController } from './books.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { UsersModule } from 'src/users/users.module';
+import { AuthorsModule } from 'src/authors/authors.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Book]), UsersModule, AuthorsModule],
   providers: [BooksService],
   controllers: [BooksController],
 })
