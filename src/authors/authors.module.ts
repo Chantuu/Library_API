e9de29b67d686b478b339c61b98ce69da3d87bcd
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-26 14:35:05 +0400

    Import UsersModule in AuthorsModule
     Imported UsersModule for using AuthGuard.

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -3,9 +3,10 @@ import { AuthorsService } from './authors.service';
 import { AuthorsController } from './authors.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Author } from './author.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Author])],
+  imports: [TypeOrmModule.forFeature([Author]), UsersModule],
   providers: [AuthorsService],
   controllers: [AuthorsController],
   exports: [AuthorsService],
