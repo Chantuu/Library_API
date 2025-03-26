6fd695e4165304e732dd9f98903eec20285c38f0
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-26 13:26:31 +0400

    Implement GET /books/:id Endpoint

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
+import { IntIdParam } from 'src/utilities/decorators/intIdParam.decorator';
+import { formatResponse } from 'src/utilities/functions/formatRepsonse';
 
 @Controller('authors')
 export class AuthorsController {
@@ -17,5 +25,23 @@ export class AuthorsController {
     @Query('page', new ParseIntPipe({ optional: true })) page?: number,
   ) {
     return await this.authorsService.getAllAuthorsPaginated(itemsOnPage, page);
+  }
+
+  /**
+   * This is a handler for GET /authors/:id endpoint. It retrieves specific Author entity
+   * based on id url paramter. It has id type validation built-in. If url param is correctly
+   * validated and Author with that id exists, that AUthor entity is returned as a response.
+   * Otherwise, correct error response is sent.
+   */
+  @Get(':id')
+  async getOneAuthor(@IntIdParam('id') authorId: number) {
+    const authorExists = await this.authorsService.findOneById(authorId);
+    if (authorExists) {
+      return formatResponse(authorExists);
+    } else {
+      throw new BadRequestException(
+        'The author with provided id does not exist. Please, provide correct id!',
+      );
+    }
   }
 }
