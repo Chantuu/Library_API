4d9709344516c99ff1bb30b16f797c9ffa05db36
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-03-26 11:39:53 +0400

    Implement GET /authors Endpoint

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
 
-@Controller('author')
-export class AuthorsController {}
+@Controller('authors')
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+
+  /**
+   * This is a handler for GET /authors endpoint. It retrieves all existing Author entities
+   * from database and returns them paginated. itemsOnPage and page are optional query parameters
+   * used to customize pagination output. It also has query parameter validation built-in.
+   */
+  @Get()
+  async getAllAuthors(
+    @Query('itemsOnPage', new ParseIntPipe({ optional: true }))
+    itemsOnPage?: number,
+    @Query('page', new ParseIntPipe({ optional: true })) page?: number,
+  ) {
+    return await this.authorsService.getAllAuthorsPaginated(itemsOnPage, page);
+  }
+}
