0922c40a9c98abe24aacbf50048758d93838250a
Chantuu <47988187+Chantuu@users.noreply.github.com>
2025-04-02 00:57:59 +0400

    Implement Basic Swagger UI Functionality

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // SwaggerUI Setup
+  const config = new DocumentBuilder()
+    .setTitle('Simple Library API')
+    .setDescription('Some Description')
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   // whitelist: true automatically strips any request body properties not present in defined DTO classes
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
